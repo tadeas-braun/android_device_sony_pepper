b476001db259f6113795cd5a023d331dc31139c5
adeel <alwaysadeel@gmail.com>
2014-03-18 20:13:16 +0500

    update prebuilt

---
+++ prebuilt/resources-480x854.mk
@@ -0,0 +1,9 @@
+#Offline charging animation
+PRODUCT_COPY_FILES += \
+     device/sony/kumquat/prebuilt/animations/charging_animation_01.png:system/semc/chargemon/data/charging_animation_01.png \
+     device/sony/kumquat/prebuilt/animations/charging_animation_02.png:system/semc/chargemon/data/charging_animation_02.png \
+     device/sony/kumquat/prebuilt/animations/charging_animation_03.png:system/semc/chargemon/data/charging_animation_03.png \
+     device/sony/kumquat/prebuilt/animations/charging_animation_04.png:system/semc/chargemon/data/charging_animation_04.png \
+     device/sony/kumquat/prebuilt/animations/charging_animation_05.png:system/semc/chargemon/data/charging_animation_05.png \
+     device/sony/kumquat/prebuilt/animations/charging_animation_06.png:system/semc/chargemon/data/charging_animation_06.png \
+     device/sony/kumquat/prebuilt/animations/charging_animation_07.png:system/semc/chargemon/data/charging_animation_07.png \
