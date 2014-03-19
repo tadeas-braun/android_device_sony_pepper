f9cc3baa10ba9ea96ee4a8f1de737d70a1fe6cca
alwaysadeel <alwaysadeel@gmail.com>
2014-03-19 07:16:51 +0500

    Update BoardConfig.mk
    take values from munjeni's tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,33 +1,21 @@
-USE_CAMERA_STUB := true
-
 # inherit from the proprietary version
 -include vendor/sony/pepper/BoardConfigVendor.mk
 
 #inherit from the common montblanc definitions
 -include device/sony/montblanc-common/BoardConfigCommon.mk
 
+# Platform specific headers
 TARGET_SPECIFIC_HEADER_PATH += device/sony/pepper/include
 
+# Kernel specific configurations
 TARGET_KERNEL_CONFIG := xperiasola_defconfig
+BOARD_KERNEL_CMDLINE := cachepolicy=writealloc noinitrd init=init board_id=1 logo.nologo root=/dev/ram0 rw rootwait console=null androidboot.console=null androidboot.hardware=st-ericsson mem=96M@0 mem_mtrace=15M@96M mem_mshared=1M@111M mem_modem=16M@112M mem=32M@128M mem_issw=1M@160M hwmem=71M@161M mem=280M@232M mpcore_wdt.mpcore_margin=359 end audit=1
 
-# Partition information
-BOARD_VOLD_MAX_PARTITIONS := 16
+# Specific recovery configuration
+BOARD_HAS_SDCARD_INTERNAL := true
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
-BOARD_CMDLINE := device/sony/pepper/config/cmdline.txt
+# Specific flag for LED bar notification
+COMMON_GLOBAL_CFLAGS += -DNEW_NOTIFICATION -DSECOND_NOTIFICATION
 
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
-BOARD_USERDATA_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_FORCE_KERNEL_ADDRESS  := 0x00008000
-BOARD_FORCE_RAMDISK_ADDRESS := 0x01000000
-
-COMMON_GLOBAL_CFLAGS += -DNEW_NOTIFICATION
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_USERIMAGES_USE_EXT4 := true
-
-BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p14
-
+# Device identifiers for updates
 TARGET_OTA_ASSERT_DEVICE := MT27i,MT27a,pepper
