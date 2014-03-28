9e8ad9e8dabb6e4cba23851ef9617c9d0e690372
tadeas482 <tadeas.braun@gmail.com>
2014-03-28 17:49:40 +0100

    pepper.mk: correct directory

--- pepper.mk
+++ pepper.mk
@@ -34,9 +34,9 @@ PRODUCT_COPY_FILES += \
 
 # NFCEE access control
 ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/prebuilt/system/etc/nfcee_access.xml
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/config/nfcee_access.xml
 else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/prebuilt/system/etc/nfcee_access_debug.xml
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/config/system/etc/nfcee_access_debug.xml
 endif
 PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
