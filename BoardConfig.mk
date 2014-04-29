e2c915382666db7a4d0c379928eb305cfae63db6
Agontuk <rifat662@gmail.com>
2014-04-29 12:27:47 +0600

    add external storage flag for TWRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,8 @@ TARGET_KERNEL_CONFIG := xperiasola_defconfig
 DEVICE_RESOLUTION := 480x854
 BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_HAS_SDCARD_INTERNAL := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TARGET_RECOVERY_FSTAB := device/sony/pepper/config/fstab.st-ericsson
 
 
